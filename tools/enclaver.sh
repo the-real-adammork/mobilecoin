23d55c2f1c3d5e50858848cc41bff444dc4862e1
the-real-adammork <adammork@gmail.com>
2021-11-16 14:55:50 -0800

    Add script to download and print enclave values for a given network

---
+++ tools/enclaver.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+NETWORK_ENVIRONMENT="$1.mobilecoin.com"
+
+echo -e "\nDownloading MrEnclave values for $NETWORK_ENVIRONMENT\n"
+export NETWORK=$NETWORK_ENVIRONMENT; ./download_sigstruct.sh 
+
+echo -e "\nConsensus Enclave"
+cargo run -p mc-sgx-css-dump --release 2>/dev/null < ./consensus-enclave.css | grep MRENCLAVE | sed 's/,//'
+echo -e "\nView Enclave"
+cargo run -p mc-sgx-css-dump --release 2>/dev/null < ./view-enclave.css | grep MRENCLAVE | sed 's/,//'
+echo -e "\nIngest Enclave"
+cargo run -p mc-sgx-css-dump --release 2>/dev/null < ./ingest-enclave.css  | grep MRENCLAVE | sed 's/,//'
+echo -e "\nLedger Enclave"
+cargo run -p mc-sgx-css-dump --release 2>/dev/null < ./ledger-enclave.css  | grep MRENCLAVE | sed 's/,//'
+echo -e "\n"
