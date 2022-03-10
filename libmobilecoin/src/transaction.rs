33b7950a20c1bc783ebd9f81e8261d68a67c8d6d
the-real-adammork <adammork@gmail.com>
2022-03-10 04:22:50 -0800

    keep in transaction.rs for simplicity

--- libmobilecoin/src/transaction.rs
+++ libmobilecoin/src/transaction.rs
@@ -1,9 +1,14 @@
 // Copyright (c) 2018-2021 The MobileCoin Foundation
 
-use crate::{common::*, fog::McFogResolver, keys::McPublicAddress, LibMcError};
+use crate::{
+    common::*,
+    fog::McFogResolver,
+    keys::McPublicAddress,
+    LibMcError, McAccountKey
+};
 use core::convert::TryFrom;
 use crc::Crc;
-use mc_account_keys::PublicAddress;
+use mc_account_keys::{PublicAddress, AccountKey};
 use mc_crypto_keys::{ReprBytes, RistrettoPrivate, RistrettoPublic};
 use mc_fog_report_validation::FogResolver;
 use mc_transaction_core::{
@@ -13,7 +18,12 @@ use mc_transaction_core::{
     tx::{TxOut, TxOutConfirmationNumber, TxOutMembershipProof},
     Amount, BlockVersion, CompressedCommitment,
 };
-use mc_transaction_std::{InputCredentials, RTHMemoBuilder, TransactionBuilder};
+use mc_transaction_std::{
+    InputCredentials,
+    RTHMemoBuilder,
+    TransactionBuilder,
+    SenderMemoCredential
+};
 use mc_util_ffi::*;
 
 /* ==== TxOut ==== */
@@ -535,6 +545,57 @@ pub extern "C" fn mc_transaction_builder_build(
     })
 }
 
+/* ==== TxOutMemoBuilder ==== */
+
+/// # Preconditions
+///
+/// * `account_key` - must be a valid `AccountKey` with `fog_info`.
+#[no_mangle]
+pub extern "C" fn mc_memo_builder_sender_and_destination_create(
+    account_key: FfiRefPtr<McAccountKey>,
+) -> FfiOptOwnedPtr<McTxOutMemoBuilder> {
+    ffi_boundary(|| {
+        let account_key = AccountKey::try_from_ffi(&account_key).expect("account_key is invalid");
+        let mut rth_memo_builder: RTHMemoBuilder = RTHMemoBuilder::default();
+        rth_memo_builder.set_sender_credential(SenderMemoCredential::from(&*account_key));
+        rth_memo_builder.enable_destination_memo();
+
+        let memo_builder_box: Box<dyn MemoBuilder + Sync + Send> = Box::new(rth_memo_builder);
+
+        Some(memo_builder_box)
+    })
+}
+
+/// # Preconditions
+///
+/// * `account_key` - must be a valid `AccountKey` with `fog_info`.
+#[no_mangle]
+pub extern "C" fn mc_memo_builder_sender_payment_request_and_destination_create(
+    payment_request_id: u64,
+    account_key: FfiRefPtr<McAccountKey>,
+) -> FfiOptOwnedPtr<McTxOutMemoBuilder> {
+    ffi_boundary(|| {
+        let account_key = AccountKey::try_from_ffi(&account_key).expect("account_key is invalid");
+        let mut rth_memo_builder: RTHMemoBuilder = RTHMemoBuilder::default();
+        rth_memo_builder.set_sender_credential(SenderMemoCredential::from(&*account_key));
+        rth_memo_builder.set_payment_request_id(payment_request_id);
+        rth_memo_builder.enable_destination_memo();
+
+        let memo_builder_box: Box<dyn MemoBuilder + Sync + Send> = Box::new(rth_memo_builder);
+
+        Some(memo_builder_box)
+    })
+}
+
+#[no_mangle]
+pub extern "C" fn mc_memo_builder_default_create(
+    ) -> FfiOptOwnedPtr<McTxOutMemoBuilder> {
+    ffi_boundary(|| {
+        let memo_builder_box: Box<dyn MemoBuilder + Sync + Send> =
+            Box::new(RTHMemoBuilder::default());
+        Some(memo_builder_box)
+    })
+}
 impl<'a> TryFromFfi<&McBuffer<'a>> for CompressedCommitment {
     type Error = LibMcError;
 
